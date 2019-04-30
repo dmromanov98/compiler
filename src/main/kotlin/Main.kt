d190410047a84bdff7c973ee32cb77c425e36bfb
Dmitry Romanov <battlebot98@gmail.com>
2019-04-30 19:17:32 +0300

    Fix gradle file

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,34 +1,14 @@
-fun main() {
-    //val lexer = Lexer()
-    Lexer.filePath = "src/main/resources/Main.txt"
-    //var str = ""
-    //lexer.getCharsFromFile().forEach { str += it.toString() }
-    //println(str)
+fun main(args: Array<String>) {
+    val portObtainedInArgs = args.isNotEmpty()
+    Lexer.filePath = if (portObtainedInArgs)
+        args[0]
+    else
+        "Main.txt"
+
     val parser = Parser(Lexer.filePath!!)
-//    while (str.length > Lexer.charPosition) {
-//        lexer.nextToken()
-//        println("{${Lexer.symb} , ${Lexer.value}}")
-//    }
-
-//    Lexer.charPosition = -1
-//    Lexer.symb = null
-//    Lexer.value = null
-
     val compiler = Compiler()
     val node = parser.parse()
     val program = compiler.compile(node)
     val virtualMachine = VirtualMachine()
-
-    //program.forEach { println(it) }
     virtualMachine.run(program)
-    //out(node)
-}
-
-fun out(node: Node?) {
-    if (node != null) {
-        println("${node.kind} ${node.value}")
-        out(node.op1)
-        out(node.op2)
-        out(node.op3)
-    }
 }
